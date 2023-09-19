efbff607d680eaf7579bda18c23a1b18e0e28bf5
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2023-09-19 14:29:20 -0400

    Update myWE2E_run.sh

--- hera/WE2E/myWE2E_run.sh
+++ hera/WE2E/myWE2E_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-python3 run_WE2E_tests.py --tests="my_expts.txt" --machine="hera" --account="fv3-cam" --use_cron_to_relaunch --cron_relaunch_intvl_mnts="03"
+python3 run_WE2E_tests.py --tests="my_expts.txt" --machine="hera" --account="fv3-cam"
